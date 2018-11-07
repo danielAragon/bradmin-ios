dcf37713e06622a33bda537d6e3a4268b60495b6
Silvana Stipetich Santillán <silvana@imac-de-silvana.local>
2018-11-07 10:03:02 -0500

    Initial Commit

---
+++ iBetterRide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBetterRide
+//
+//  Created by Silvana Stipetich Santillán on 11/7/18.
+//  Copyright © 2018 Better Ride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
